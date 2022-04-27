85d2170521593f59ff81666150d13c0fa46b5261
Samuel <60421298+iamsamuelhere@users.noreply.github.com>
2022-04-27 14:21:47 +0530

    Update hero.tsx

--- src/home/hero.tsx
+++ src/home/hero.tsx
@@ -7,22 +7,19 @@ const HeroHolder = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  row-gap: 20px;
 
   & > * {
     flex-basis: 50%;
   }
 
   h3 {
-    margin-bottom: -10px;
     font-weight: normal;
     text-align: center;
   }
 
-  h1 {
-    font-size: 2rem;
-    margin-bottom: 2rem;
+  p {
     text-align: center;
+    font-size: 2rem;
   }
 
   .cta-holder {
@@ -33,17 +30,8 @@ const HeroHolder = styled.div`
   @media (min-width: 768px) {
     flex-direction: row;
 
-    h3 {
-      text-align: start;
-    }
-
-    h1 {
-      font-size: 2.5rem;
-      text-align: start;
-    }
-
     .cta-holder {
-      justify-content: flex-start;
+      justify-content: center;
     }
   }
 `;
@@ -61,11 +49,11 @@ const Hero: NextPage = () => {
   return (
     <HeroHolder>
       <div>
-        <h3>Free & Open source</h3>
-        <h1>
-          Single Page <br />
-          Resume Builder
-        </h1>
+        <p style={{ color: '#6C63FF', fontSize: '5rem', fontWeight: 'bold' }}>CeFolio</p>
+        <p style={{ fontSize: '1.5rem' }}>
+          A <span style={{ color: '#6C63FF', fontSize: '1.5rem' }}>Link</span> is all you need
+        </p>
+        <br />
         <div className="cta-holder">
           <Link href="/editor">
             <a>
